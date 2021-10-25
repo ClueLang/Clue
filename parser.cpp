ec7ae0c9aa002b595903d41a5d544e817fb1e0df
Felix44 <felice.dangelo2013@gmail.com>
2021-10-26 00:14:20 +0200

    added working and messy if compilation

--- parser.cpp
+++ parser.cpp
@@ -5,18 +5,51 @@
 #include <stack>
 #include <vector>
 #define ERROR(msg) throw std::string("Error in file \"" + filename + "\" at line [" + std::to_string(line) + "]: " + msg)
+#define SPACEIF(msg) !spaced ? std::string(std::string(" ") + msg).c_str() : msg
+#define PARSETOKEN \
+	prevcompiled = compiling; \
+	switch (str2hash(prevcompiled.c_str())) { \
+		case str2hash("if"): { \
+			scope.push(token(line, TOKEN_IF)); \
+			fprintf(output, "%s", "if"); \
+			break; \
+		} \
+		default: { \
+			fprintf(output, "%s", prevcompiled.c_str()); \
+			break; \
+		} \
+	} \
+	compiling.clear();
+
 
 std::string codepath;
 std::unordered_map<std::string, std::string> files;
+
+enum token_type {
+	TOKEN_DO,
+	TOKEN_IF,
+	TOKEN_WHILE,
+	TOKEN_UNTIL,
+};
+
+struct token {
+	unsigned int line;
+	token_type type;
+	token(unsigned int line, token_type type) :
+    	line(line),
+    	type(type)
+	{}
+};
 
 constexpr unsigned int str2hash(const char *str, int h = 0) {
 	return !str[h] ? 5381 : (str2hash(str, h + 1) * 33) ^ str[h];
 }
 
 void compilefile(std::string path, std::string filename) {
+	bool spaced = false;
 	unsigned int pscope = 0, line = 1;
 	std::string prevcompiled, compiling;
-	std::stack<unsigned int> scope;
+	std::stack<token> scope, inscope;
 	
 	std::string compiledname = filename == "main.clue" ? "main.lua" : std::string(std::to_string(files.size()) + ".lua");
 	files[filename] = compiledname;
@@ -35,13 +68,22 @@ void compilefile(std::string path, std::string filename) {
 	int byte;
 	while ((byte = fgetc(code)) != EOF) {
 		char crt = byte;
+		printf("%s\n", spaced ? "true" : "false");
 		switch (crt) {
 			case '\t': {
+				fprintf(output, "\t");
+				spaced = true;
+				continue;
+			}
+			case 13: {
+				spaced = true;
 				continue;
 			}
 			case '\n': {
+				PARSETOKEN
 				line++;
 				fprintf(output, "\n");
+				spaced = true;
 				continue;
 			}
 			case '(': {
@@ -49,32 +91,47 @@ void compilefile(std::string path, std::string filename) {
 				break;
 			}
 			case ')': {
-				if (pscope <= 0) ERROR("Unexpected token \")\" found.");
+				if (pscope <= 0) ERROR("Unexpected token \")\".");
 				pscope--;
 				break;
 			}
 			case '{': {
-				//check stack
-				continue;
-			}
-			case '}': {
-				//check stack
-				continue;
-			}
-			case ' ': {
-				prevcompiled = compiling;
-				compiling.clear();
-				//examine the token
-				switch (str2hash(prevcompiled.c_str())) {
+				PARSETOKEN
+				if (scope.empty()) ERROR("Unexpected token \"{\".");
+				token poptoken = scope.top();
+				scope.pop();
+				inscope.push(poptoken);
+				switch (poptoken.type) {
+					case TOKEN_IF: {
+						fprintf(output, "%s", SPACEIF("then"));
+						break;
+					}
 					default: {
-						fprintf(output, "%s ", prevcompiled.c_str());
+						ERROR("Unknown token in stack.");
 						break;
 					}
 				}
+				spaced = false;
+				continue;
+			}
+			case '}': {
+				PARSETOKEN
+				if (inscope.empty()) ERROR("Unexpected token \"}\".");
+				inscope.pop();
+				fprintf(output, "%s", SPACEIF("end"));
+				spaced = false;
+				continue;
+			}
+			case ' ': {
+				printf("%d ", (int)crt);
+				PARSETOKEN
+				fprintf(output, " ");
+				spaced = true;
 				continue;
 			}
 		}
-		compiling += crt;
+		spaced = false;
+		if (crt) compiling += crt;
 	}
 	fprintf(output, "%s", compiling.c_str());
 	fclose(output);
