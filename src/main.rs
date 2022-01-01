378472bea82e3c66339f3d2ba5060026465cfc86
Felix44 <felice.dangelo2013@gmail.com>
2022-01-02 00:27:27 +0100

    added a bunch of stuff

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,34 @@
 
 use std::io;
 use std::env;
+use std::fs;
+use std::path::Path;
+use std::process;
+
+fn error(msg: &str) {
+	println!("{}", &msg);
+	process::exit(1)
+}
+
+fn CompileFolder(path: String) {
+	for entry in fs::read_dir(path) {
+
+	}
+}
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let mut path: String = String::new();
-    if args.len() == 1 {
-        println!("Please insert path to code directory: ");
-        io::stdin()
-            .read_line(&mut path)
-            .expect("Failed to read path!");
-    } else {
-        path = args[1].clone();
-    }
-    
+	let args: Vec<String> = env::args().collect();
+	let mut codepath: String = String::new();
+	if args.len() == 1 {
+		println!("Please insert path to code directory: ");
+		io::stdin()
+			.read_line(&mut codepath)
+			.expect("Failed to read path!");
+	} else {
+		codepath = args[1].clone();
+	}
+	if !Path::new(&codepath).is_dir() {error("Given path is invalid!");}
+	fs::create_dir(codepath.clone() + "\\.clue")
+		.expect("Failed to create output directory!");
+		CompileFolder(codepath);
 }
