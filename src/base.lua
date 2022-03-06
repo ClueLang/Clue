21365c59243756730b9a7c93635e7eecf298d3c4
Felix44 <felice.dangelo2013@gmail.com>
2022-03-06 17:17:30 +0100

    cache nil values

--- src/base.lua
+++ src/base.lua
@@ -2,13 +2,15 @@ local require, modules
 do
 	local _require = _G.require
 	local cache = {}
+	local nils = {}
 	function require(modname)
+		if nils[modname] then return end
 		local cached = cache[modname]
 		if cached ~= nil then return cached end
 		cached = modules[modname]
 		if cached ~= nil then
-			cached = cached()
-			cache[modname] = cached
+			cached = cached();
+			(cached ~= nil and cache or nils)[modname] = cached
 			return cached
 		end
 		return _require(modname)
