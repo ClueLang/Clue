2e389cf9ae09386b59113ba25eeeeca4e3f34b51
Felix44 <felice.dangelo2013@gmail.com>
2022-03-08 19:54:44 +0100

    use clap for parsing arguments

--- src/main.rs
+++ src/main.rs
@@ -10,26 +10,82 @@ macro_rules! check {
 	};
 }
 
-mod options;
 mod scanner;
 mod parser;
 mod compiler;
 
-use std::env;
+use clap::Parser;
 use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
 use std::time::Instant;
 use std::path::Path;
-use options::*;
 use scanner::*;
 use parser::*;
 use compiler::*;
 
-pub static mut output: String = String::new();
+pub static mut finaloutput: String = String::new();
+
+static mut UNSAFE_TOKENS: bool = false;
+static mut UNSAFE_STRUCT: bool = false;
+static mut UNSAFE_OUTPUT: bool = false;
+static mut UNSAFE_NOJITBIT: bool = false;
+static mut UNSAFE_CONTINUE: bool = false;
+static mut UNSAFE_DONTSAVE: bool = false;
+static mut UNSAFE_PATHISCODE: bool = false;
+static mut UNSAFE_RAWSETGLOBALS: bool = false;
+pub static ENV_TOKENS: &bool = unsafe {&UNSAFE_TOKENS};
+pub static ENV_STRUCT: &bool = unsafe {&UNSAFE_STRUCT};
+pub static ENV_OUTPUT: &bool = unsafe {&UNSAFE_OUTPUT};
+pub static ENV_NOJITBIT: &bool = unsafe {&UNSAFE_NOJITBIT};
+pub static ENV_CONTINUE: &bool = unsafe {&UNSAFE_CONTINUE};
+pub static ENV_DONTSAVE: &bool = unsafe {&UNSAFE_DONTSAVE};
+pub static ENV_PATHISCODE: &bool = unsafe {&UNSAFE_PATHISCODE};
+pub static ENV_RAWSETGLOBALS: &bool = unsafe {&UNSAFE_RAWSETGLOBALS};
+
+#[derive(Parser)]
+#[clap(about, version = "b2.1.89", long_about = None)]
+struct Cli {
+	/// The path to the directory where the *.clue files are located.
+	/// Every directory inside the given directory will be checked too.
+	/// If the path points to a single *.clue file, only that file will be compiled.
+	path: String,
+
+	/// Print list of detected tokens in compiled files
+	#[clap(long)]
+	tokens: bool,
+
+	/// Print syntax structure of the tokens of the compiled files
+	#[clap(long)]
+	r#struct: bool,
+
+	/// Print output Lua code in the console
+	#[clap(long)]
+	output: bool,
+
+	/// Don't use LuaJIT's bit library for bitwise operations
+	#[clap(short, long)]
+	nojitbit: bool,
+
+	/// Don't use tags for continue
+	#[clap(short, long)]
+	r#continue: bool,
+
+	/// Don't save compiled code
+	#[clap(short, long)]
+	dontsave: bool,
+
+	/// Treat PATH not as a path but as Clue code
+	#[clap(short, long)]
+	pathiscode: bool,
+
+	/// Use rawset to create globals
+	#[clap(short, long)]
+	rawsetglobals: bool,
+}
 
 fn AddToOutput(string: &str) {
-	unsafe {output += string}
+	unsafe {finaloutput += string}
 }
 
 fn CompileCode(code: String, name: String, scope: usize) -> Result<String, String> {
@@ -43,7 +99,7 @@ fn CompileCode(code: String, name: String, scope: usize) -> Result<String, Strin
 		println!("Parsed structure of file \"{}\":\n{:#?}", name, ctokens);
 	}
 	let code = CompileTokens(scope, ctokens);
-	if *ENV_PRINTOUT {
+	if *ENV_OUTPUT {
 		println!("Compiled Lua code of file \"{}\":\n{}", name, code);
 	}
 	println!("Compiled file \"{}\" in {} seconds!", name, time.elapsed().as_secs_f32());
@@ -75,40 +131,18 @@ fn CompileFolder(path: &Path, rpath: String) -> Result<(), String> {
 }
 
 fn main() -> Result<(), String> {
-	let args: Vec<String> = env::args().collect();
-	SetupEnv();
-	let codepath;
-	if args.len() == 1 || args.contains(&String::from("-help")) {
-		println!(
-"Clue transpiler (BETA)
-Made by Felix44
-https://github.com/Felix-44/Clue
-
-USAGE:
-	clue -help		Send this very message
-	clue -version 		Send this transpiler's version
-	clue [PATH] [OPTIONS]	Compile any *.clue file found in the given directory
-
-PATH:
-	The path to the directory where the *.clue files are located.
-	Every directory inside the given directory will be checked too.
-	If the path points to a single *.clue file, only that file will be compiled.
-
-OPTIONS:
-	-tokens		Print list of detected tokens in compiled files
-	-struct 	Print syntax structure of the tokens of the compiled files
-	-printout	Print output Lua code in the console
-	-nojitbit	Don't use LuaJIT's bit library for bitwise operations
-	-continue	Don't use tags for continue
-	-dontsave	Don't save compiled code
-	-pathiscode 	Treat PATH not as a path but as Clue code");
-		return Ok(());
+	let cli = Cli::parse();
+    unsafe {
+		UNSAFE_TOKENS = cli.tokens;
+		UNSAFE_STRUCT = cli.r#struct;
+		UNSAFE_OUTPUT = cli.output;
+		UNSAFE_NOJITBIT = cli.nojitbit;
+		UNSAFE_CONTINUE = cli.r#continue;
+		UNSAFE_DONTSAVE = cli.dontsave;
+		UNSAFE_PATHISCODE = cli.pathiscode;
+		UNSAFE_RAWSETGLOBALS = cli.rawsetglobals;
 	}
-	codepath = &args[1];
-	if codepath == "-version" {
-		println!("Version b2.1.87");
-		return Ok(());
-	}
+	let codepath = &cli.path;
 	if *ENV_PATHISCODE {
 		let code = CompileCode(codepath.clone(), String::from("(command line)"), 0)?;
 		println!("{}", code);
@@ -121,14 +155,14 @@ OPTIONS:
 		AddToOutput("\r}\nrequire(\"main\")");
 		if !*ENV_DONTSAVE {
 			let compiledname = String::from(path.display().to_string()) + "\\main.lua";
-			check!(fs::write(compiledname, unsafe {&output}))
+			check!(fs::write(compiledname, unsafe {&finaloutput}))
 		}
 	} else if path.is_file() {
 		let code = CompileFile(path, path.file_name().unwrap().to_string_lossy().into_owned(), 0)?;
 		AddToOutput(&code);
 		if !*ENV_DONTSAVE {
 			let compiledname = String::from(path.display().to_string().strip_suffix(".clue").unwrap()) + ".lua";
-			check!(fs::write(compiledname, unsafe {&output}))
+			check!(fs::write(compiledname, unsafe {&finaloutput}))
 		}
 	} else {
 		return Err(String::from("The given path doesn't exist"));
