459325c2e96c7519d568ba7f65edd64d21963777
Enn3Developer <arturpeshko39@gmail.com>
2022-06-17 10:52:36 +0200

    Removed `unsafe`

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+use std::sync::RwLock;
+
+use lazy_static::lazy_static;
+
+use crate::env::EnvData;
+
+pub mod compiler;
+pub mod env;
+pub mod parser;
+pub mod scanner;
+
+lazy_static! {
+	pub static ref ENV_DATA: RwLock<EnvData> = RwLock::new(EnvData::new());
+}
+
+#[macro_export]
+macro_rules! check {
+	($tocheck: expr) => {
+		match $tocheck {
+			Ok(t) => t,
+			Err(e) => return Err(e.to_string()),
+		}
+	};
+}
