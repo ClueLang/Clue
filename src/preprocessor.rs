1cd3a8ef7143d1e38a293ee8d5ef56f6d1afbd9e
Maiori44 <felice.dangelo2013@gmail.com>
2022-11-22 11:38:35 +0100

    Unfinished base for @ifdef

--- src/preprocessor.rs
+++ src/preprocessor.rs
@@ -1,12 +1,50 @@
-use std::{iter::Peekable, str::Chars};
+use std::{iter::Peekable, str, str::Chars, env};
 
 use crate::format_clue;
 
 pub type LinkedString = std::collections::LinkedList<char>;
 
-fn error(msg: String, line: usize, filename: &String) -> String {
+fn error(msg: impl Into<String>, line: usize, filename: &String) -> String {
 	println!("Error in file \"{filename}\" at line {line}!");
-	msg
+	msg.into()
+}
+
+fn expected(expected: &str, got: &str, line: usize, filename: &String) -> String {
+	error(format_clue!("Expected '", expected, "', got '", got, "'"), line, filename)
+}
+
+fn expected_before(expected: &str, before: &str, line: usize, filename: &String) -> String {
+	error(format_clue!("Expected '", expected, "' before '", before, "'"), line, filename)
+}
+
+fn skip_whitespace(chars: &mut Peekable<Chars>) {
+	while {
+		if let Some(c) = chars.peek() {
+			c.is_whitespace()
+		} else {
+			false
+		}
+	} {
+		chars.next();
+	}
+}
+
+fn reach(
+	chars: &mut Peekable<Chars>,
+	end: char,
+	line: usize,
+	filename: &String
+) -> Result<(), String> {
+	skip_whitespace(chars);
+	if let Some(c) = chars.next() {
+		if end != c {
+			Err(expected(&end.to_string(), &c.to_string(), line, filename))
+		} else {
+			Ok(())
+		}
+	} else {
+		Err(expected_before(&end.to_string(), "<end>", line, filename))
+	}
 }
 
 fn read_word(chars: &mut Peekable<Chars>) -> String {
@@ -23,6 +61,27 @@ fn read_word(chars: &mut Peekable<Chars>) -> String {
 	word
 }
 
+fn assert_word(chars: &mut Peekable<Chars>, line: usize, filename: &String) -> Result<String, String> {
+	skip_whitespace(chars);
+	let word = read_word(chars);
+	if word.is_empty() {
+		Err(error("Word expected", line, filename))
+	} else {
+		Ok(word)
+	}
+}
+
+fn read_block(
+	mut chars: &mut Peekable<Chars>,
+	mut line: usize,
+	filename: &String
+) -> Result<(), String> {
+	reach(chars, '{', line, filename)?;
+	let partiions: (LinkedString, LinkedString) = chars.partition(|c| {true});
+	chars = &mut partiions.0.into_iter().peekable();
+	Ok(())
+}
+
 pub fn preprocess_code(rawcode: String, filename: &String) -> Result<LinkedString, String> {
 	let mut code = LinkedString::new();
 	let mut line = 1usize;
@@ -33,7 +92,12 @@ pub fn preprocess_code(rawcode: String, filename: &String) -> Result<LinkedStrin
 			'\n' => line += 1,
 			'@' => {
 				let directive = read_word(chars);
-				match directive {
+				match directive.as_str() {
+					"ifdef" => {
+						let var = assert_word(chars, line, filename)?;
+
+					}
+					"" => return Err(error("Expected directive name", line, filename)),
 					_ => return Err(error(
 						format_clue!("Unknown directive '", directive, "'"),
 						line, filename
