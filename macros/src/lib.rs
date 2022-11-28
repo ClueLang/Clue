bc6ab076689f0b2d34dbffa4ef21bfb575e70d88
Maiori44 <felice.dangelo2013@gmail.com>
2022-11-28 20:54:05 +0100

    Workspace refactor (BIG WIP)

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,1 +1,31 @@
-//ADD STUFF LATER
+extern crate proc_macro;
+
+#[cfg(feature = "map")]
+extern crate litrs;
+
+#[cfg(feature = "version")]
+extern crate toml;
+
+#[cfg(any(feature = "map", feature = "version"))]
+use proc_macro::TokenStream;
+
+#[cfg(feature = "map")]
+#[proc_macro]
+pub fn generate_map(item: TokenStream) -> TokenStream {
+	let input = item.into_iter().collect::<Vec<_>>();
+	//println!("{:?}", input.attrs);
+	"".parse().unwrap()
+}
+
+#[cfg(feature = "version")]
+#[proc_macro]
+pub fn clue_version(_item: TokenStream) -> TokenStream {
+	use toml::Value;
+	format!(
+		"\"{}\"",
+		include_str!("../../core/Cargo.toml")
+			.parse::<Value>().unwrap()["package"]
+			.as_table().unwrap()["version"]
+			.as_str().unwrap()
+	).parse().unwrap()
+}
