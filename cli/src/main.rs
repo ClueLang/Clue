13c658f665a1358bce05b5a7474f422d7d2aae84
Maiori44 <felice.dangelo2013@gmail.com>
2023-02-05 09:36:54 +0100

    fix some of the directory compiling things being exposed as a library

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,30 +1,21 @@
-use ahash::AHashMap;
 use clap::{crate_version, Parser};
-use clue::{check_for_files, wait_threads, CodeQueue, PreprocessorAnalyzerData, ThreadData};
 use clue_core::{
 	check,
-	code::*,
 	compiler::*,
 	env::{ContinueMode, Options},
-	format_clue,
 	parser::*,
 	preprocessor::*,
 	scanner::*,
 };
-
-use flume::Sender;
-use std::cmp::min;
-
-use crossbeam_queue::SegQueue;
+use threads::compile_folder;
 use std::{
-	ffi::OsStr,
-	fmt::{Display, Write},
+	fmt::Write,
 	fs,
 	path::Path,
-	sync::Arc,
-	thread,
 	time::Instant,
 };
+
+mod threads;
 
 #[derive(Parser)]
 #[clap(
@@ -117,7 +108,7 @@ struct Cli {
 	execute: bool,
 }
 
-fn compile_code(
+pub fn compile_code(
 	codes: PPCode,
 	variables: &PPVars,
 	name: &String,
@@ -155,174 +146,6 @@ fn compile_code(
 		time.elapsed().as_secs_f32()
 	);
 	Ok((code, statics))
-}
-
-fn compile_folder<P: AsRef<Path>>(
-	file_path: P,
-	rpath: String,
-	options: &Options,
-) -> Result<(String, String), String>
-where
-	P: AsRef<OsStr> + Display,
-{
-	let files = check!(check_for_files(file_path, rpath));
-	let files_len = files.len();
-	let threads_count = min(files_len, num_cpus::get() * 2);
-	let codes = SegQueue::new();
-	let files = Arc::new(files);
-	let mut errored = 0;
-	let mut variables = vec![];
-	let mut output = String::with_capacity(files_len * 512) + "\n";
-	let mut statics = String::with_capacity(512);
-
-	let (tx, rx) = flume::unbounded();
-
-	let mut threads = Vec::with_capacity(threads_count);
-
-	for _ in 0..threads_count {
-		// this `.clone()` is used to create new pointers
-		// that can be used from inside the newly created thread
-		let files = files.clone();
-		let tx = tx.clone();
-
-		let thread = thread::spawn(move || preprocess_file_dir(files, tx));
-
-		threads.push(thread);
-	}
-
-	wait_threads(threads);
-
-	while let Ok(data) = rx.try_recv() {
-		if data.errored {
-			errored += 1;
-			continue;
-		}
-
-		variables.push(data.variables);
-		codes.push(data.codes);
-	}
-
-	match errored {
-		0 => {}
-		1 => return Err(String::from("1 file failed to compile!")),
-		n => return Err(format!("{n} files failed to compile!")),
-	}
-
-	let variables = Arc::new(
-		variables
-			.iter()
-			.flat_map(|file_variables| file_variables.to_owned())
-			.collect::<AHashMap<Code, PPVar>>(),
-	);
-
-	let mut threads = Vec::with_capacity(threads_count);
-	let (tx, rx) = flume::unbounded();
-	let codes = Arc::new(codes);
-
-	for _ in 0..threads_count {
-		let tx = tx.clone();
-		let options = options.clone();
-		let codes = codes.clone();
-		let variables = variables.clone();
-
-		let thread = thread::spawn(move || compile_file_dir(tx, &options, codes, variables));
-
-		threads.push(thread);
-	}
-
-	wait_threads(threads);
-
-	while let Ok(data) = rx.try_recv() {
-		if data.errored {
-			errored += 1;
-			continue;
-		}
-
-		output += &data.output;
-		statics += &data.static_vars;
-	}
-
-	match errored {
-		0 => Ok((output.chars().collect(), statics.chars().collect())),
-		1 => Err(String::from("1 file failed to compile!")),
-		n => Err(format!("{n} files failed to compile!")),
-	}
-}
-
-fn compile_file_dir(
-	tx: Sender<ThreadData>,
-	options: &Options,
-	codes: Arc<CodeQueue>,
-	variables: Arc<AHashMap<Code, PPVar>>,
-) {
-	loop {
-		let (codes, realname) = match codes.pop() {
-			None => break,
-			Some((codes, realname)) => (codes, realname),
-		};
-
-		let (code, static_vars) = match compile_code(codes, &variables, &realname, 2, options) {
-			Ok(t) => t,
-			Err(e) => {
-				tx.send(ThreadData {
-					errored: true,
-					output: "".to_owned(),
-					static_vars: "".to_owned(),
-				})
-				.unwrap();
-				println!("Error: {}", e);
-				continue;
-			}
-		};
-
-		let string = format_clue!(
-			"\t[\"",
-			realname.strip_suffix(".clue").unwrap(),
-			"\"] = function()\n",
-			code,
-			"\n\tend,\n"
-		);
-
-		tx.send(ThreadData {
-			errored: false,
-			output: string,
-			static_vars,
-		})
-		.unwrap();
-	}
-}
-
-fn preprocess_file_dir(
-	files: Arc<SegQueue<(String, String)>>,
-	tx: Sender<PreprocessorAnalyzerData>,
-) {
-	loop {
-		let (filename, realname) = match files.pop() {
-			None => break,
-			Some((filename, realname)) => (filename, realname),
-		};
-
-		let (file_codes, file_variables) = match read_file(&filename, &filename) {
-			Ok(t) => t,
-			Err(e) => {
-				tx.send(PreprocessorAnalyzerData {
-					errored: true,
-					codes: Default::default(),
-					variables: Default::default(),
-				})
-				.unwrap();
-				println!("Error: {}", e);
-				continue;
-			}
-		};
-
-		tx.send(PreprocessorAnalyzerData {
-			errored: false,
-			codes: (file_codes, realname),
-			variables: file_variables,
-		})
-		.unwrap();
-	}
 }
 
 #[cfg(feature = "mlua")]
