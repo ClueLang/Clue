45fd17da6c689430a5160661959b4e451a40ce99
Maiori44 <felice.dangelo2013@gmail.com>
2023-03-06 18:08:02 +0100

    remove updater

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,12 +1,8 @@
 use clap::{Parser, Subcommand};
 use builder::*;
-use clue_core::check;
-use curl::easy::Easy;
-use updater::*;
 
 mod threads;
 mod builder;
-mod updater;
 
 #[derive(Parser)]
 #[clap(
@@ -27,24 +23,6 @@ enum Commands {
 
 	/// Compile the given file/directory
 	Build(BuildArgs),
-
-	/// Update the compiler
-	Update(UpdateOptions)
-}
-
-pub fn curl(url: &str) -> Result<String, String> {
-	let mut handle = Easy::new();
-	let mut output = Vec::new();
-	check!(handle.url(url));
-	{
-		let mut transfer = handle.transfer();
-		check!(transfer.write_function(|data| {
-			output.extend_from_slice(data);
-			Ok(data.len())
-		}));
-		check!(transfer.perform());
-	}
-	Ok(check!(String::from_utf8(output)))
 }
 
 fn main() -> Result<(), String> {
@@ -56,7 +34,6 @@ fn main() -> Result<(), String> {
 			Ok(())
 		}
 		Build(args) => build(args),
-		Update(args) => update(args),
 	}
 }
 
