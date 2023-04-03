e8d072d57786ddddbfb2c5726822adc18f7a4da9
MarkosTh09 <markos.theocharis2009@gmail.com>
2023-04-03 20:59:29 +0400

    docs(env): document 3.x structs

--- core/src/env.rs
+++ core/src/env.rs
@@ -6,13 +6,24 @@ use clap::ValueEnum;
 #[derive(Default, Debug, Copy, Clone, PartialEq, Eq, ValueEnum)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[clap(rename_all = "verbatim")]
+/// The continue mode to use when compiling `continue` keywords
 pub enum ContinueMode {
 	#[default]
 	#[clap(name = "simple")]
+	/// Simple: This mode uses the native continue keyword.
+	/// This can only be used in implementations which support it.
 	Simple,
+	/// DEPRECATED
+	///
+	/// LuaJIT: Same as `Goto`, only for compatibility reasons
 	LuaJIT,
 	#[clap(name = "goto")]
+	/// Goto: Clue will use goto continue; and a ::continue:: label when compiling `continue` keywords
+	/// instead of assuming the version of Lua you're compiling to has a proper continue keyword.
+	/// This will work with most versions of lua (lua 5.2, luajit, blua)
 	Goto,
+	/// Moonscript: This approach is guaranteed to work with any version of lua although
+	/// it has a performance impact because it uses a loop.
 	MoonScript,
 }
 
@@ -36,39 +47,67 @@ pub enum LuaSTD {
 #[derive(Default, Debug, Copy, Clone, PartialEq, Eq, ValueEnum)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[clap(rename_all = "verbatim")]
+/// The Lua version to target
 pub enum LuaVersion {
 	#[default]
+	/// LuaJIT
 	LuaJIT,
+	// Lua 5.4
 	Lua54,
+	// Lua 5.3
 	Lua53,
+	// Lua 5.2
 	Lua52,
+	// Lua 5.1
 	Lua51,
+	// BLUA
 	BLUA,
 }
 
 #[derive(Default, Debug, Copy, Clone, PartialEq, Eq, ValueEnum)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[clap(rename_all = "verbatim")]
+/// The mode to use for bitwise operations
 pub enum BitwiseMode {
 	#[default]
+	/// Clue: This mode uses the bitwise operators from Clue as is with no change
+	/// Works in BLUA
 	Clue,
 	#[clap(name = "library")]
+	/// Library: This mode uses the bits library to perform bitwise operations
+	/// Works in LuaJIT
 	Library,
+	/// Vanilla: This mode uses the bitwise operators from standard Lua
+	/// Works in Lua 5.3+
 	#[clap(name = "vanilla")]
 	Vanilla,
 }
 
 #[derive(Debug, Default, Clone)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+/// The compiler options for Clue
 pub struct Options {
+	/// Prints the tokens to stdout
 	pub env_tokens: bool,
+	/// Prints the AST to stdout
 	pub env_struct: bool,
+	/// DEPRECATED
+	///
+	/// The name of the varible the bits library is assigned to
 	pub env_jitbit: Option<String>,
+	/// The mode to use for bitwise operations
 	pub env_bitwise: BitwiseMode,
+	/// The continue mode to use when compiling `continue` keywords
 	pub env_continue: ContinueMode,
+	/// Whether to use rawset(_G, ...) instead of _G.x = ...
 	pub env_rawsetglobals: bool,
+	/// Whether to print debug information
 	pub env_debug: bool,
+	/// Whether to print the output to stdout
 	pub env_output: bool,
+	/// The Lua version to target
 	pub env_target: Option<LuaVersion>,
+	/// The path to the output file
 	pub env_targetos: String,
 	//pub env_types: TypesMode,
 	//pub env_std: LuaSTD,
