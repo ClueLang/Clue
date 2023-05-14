e6f85c9de5027205489044b7065e48d1ae9c4079
Maiori44 <felice.dangelo2013@gmail.com>
2023-05-14 11:46:46 +0200

    dont change error message once an error was thrown

--- cli/src/debug.lua
+++ cli/src/debug.lua
@@ -1,6 +1,7 @@
 local _clueline, _errored, _errored_file = 0
 
 local function _clue_error(err)
+	if _errored then return end
 	_errored = ("%s:%d: %s"):format(_errored_file, _clueline, err:match(".+: (.-)$"))
 end
 
